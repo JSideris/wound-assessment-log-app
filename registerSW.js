f357aed34d5a4cfdb1134dccc2a1c5d3b9fd0d0d
Joshua Sideris <josh.sideris@gmail.com>
2026-01-22 11:15:24 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/would-assessment-log-app/sw.js', { scope: '/would-assessment-log-app/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/wound-assessment-log-app/sw.js', { scope: '/wound-assessment-log-app/' })})}
